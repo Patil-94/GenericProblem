0173fb08b0d65cd2dd64775b0aa3a26555501677
Patil-94 <ankitakhairnar10@.com>
2021-05-02 12:30:27 +0530

    [Ankita] Add:Find Maximum Number Between Three String

--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -8,6 +8,10 @@ namespace GenericsProgram
 {
     class Program
     {
+        /// <summary>
+        /// Entery point
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             
@@ -17,13 +21,19 @@ namespace GenericsProgram
             //Console.WriteLine(MaximumNumbers.MaxIntNumber(786, 60, 212));
             //Console.WriteLine(MaximumNumbers.MaxIntNumber(70, 733, 212));
             //Console.WriteLine(MaximumNumbers.MaxIntNumber(943, 73, 3312));
-           
+
 
             ///Float values
-            
-            Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.6f, 6.5f, 2.2f));
-            Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.6f, 66.5f, 3f));
-            Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.46f, 65.5f, 231.2f));
+
+            //Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.6f, 6.5f, 2.2f));
+            //Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.6f, 66.5f, 3f));
+            //Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.46f, 65.5f, 231.2f));
+
+
+            ///string 
+            Console.Write("Maximum String is : ");
+            string StringOutput = MaximumNumbers.StringMaximumNumber("Apple", "Peach", "Banana"); 
+            Console.Write(StringOutput);
 
             Console.Read();
         }
