2a6241b882d446b0a16ad2fcd1e75a634c994953
Patil-94 <ankitakhairnar10@.com>
2021-05-02 11:48:35 +0530

    [Ankita] Add:Find Maximum Number Between  Three Int

--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -10,8 +10,13 @@ namespace GenericsProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Find Maximum Problem using Generics");
+            Console.WriteLine("Welcome To Find MaximumProblem using Generics");
+            Console.WriteLine(MaximumNumbers.MaxIntNumber(786, 60, 212));
+            Console.WriteLine(MaximumNumbers.MaxIntNumber(70, 733, 212));
+            Console.WriteLine(MaximumNumbers.MaxIntNumber(943, 73, 3312));
+            Console.Read();
         }
     }
-    
 }
+    
+
---
+++ MSTestMaxNumber/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestMaxNumber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestMaxNumber")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1cd31b77-b50b-4f9f-a713-b2ab67b73770")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
